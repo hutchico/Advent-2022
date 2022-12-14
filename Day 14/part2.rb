4716bdf2a974d652ae4ab859e1712a7ba3164c72
hutchico <22420744+hutchico@users.noreply.github.com>
2022-12-13 23:25:18 -0800

    day 14 commit

---
+++ Day 14/part2.rb
@@ -0,0 +1,149 @@
+#!/usr/bin/ruby -w
+
+class Grain
+    def initialize
+        @xcoord = 500
+        @ycoord = 0
+    end
+
+    def to_s
+        "o" #vis aid
+    end
+
+    def getx
+        return @xcoord
+    end
+
+    def gety
+        return @ycoord
+    end
+
+    def can_move?(grid) #test if this should be at rest
+        return open_down?(grid) || open_left?(grid) || open_right?(grid) ? true : false
+    end
+
+    def move(grid)
+        grid[@ycoord][@xcoord] = '.'
+        if open_down?(grid)
+            #do_nothing
+        elsif open_left?(grid) 
+            @xcoord -= 1
+        elsif open_right?(grid)
+            @xcoord += 1
+        end
+        @ycoord += 1
+        grid[@ycoord][@xcoord] = self
+    end
+
+    private
+    def open_down?(grid)
+        return grid[@ycoord+1][@xcoord] == '.' ? true : false
+    end
+
+    def open_left?(grid)
+        return grid[@ycoord+1][@xcoord-1] == '.' ? true : false
+    end
+
+    def open_right?(grid)
+        return grid[@ycoord+1][@xcoord+1] == '.' ? true : false
+    end
+
+end
+
+def drop_sand(grid)
+    count = 0
+    while true #oh the horror
+        sand = Grain.new
+        count += 1
+        grid[0][500] = sand
+        if sand.can_move?(grid)
+            sand.move(grid)
+        end
+        while true
+            if sand.gety == 0
+                break
+            end
+            if sand.can_move?(grid)
+                sand.move(grid)
+            else
+                break
+            end
+        end
+        if sand.gety == 0
+            break
+        end
+    end
+    return count
+end
+
+def draw_line(grid,pt1,pt2)
+    if pt1[0] - pt2[0] == 0 #vertical line
+        lower = pt1[1] < pt2[1] ? pt1[1] : pt2[1] #to make range notation work
+        upper = pt1[1] < pt2[1] ? pt2[1] : pt1[1]
+        for i in lower..upper
+            grid[i][pt1[0]] = '#'
+        end
+    else #horizontal line
+        lower = pt1[0] < pt2[0] ? pt1[0] : pt2[0] #to make range notation work
+        upper = pt1[0] < pt2[0] ? pt2[0] : pt1[0]
+        for i in lower..upper
+            grid[pt1[1]][i] = '#'
+        end
+    end
+end
+#DEBUG
+def draw_grid(grid)
+    for i in $window_miny..$window_maxy+3#for i in 0..grid.size - 1
+        for j in $window_minx - 65..$window_maxx + 65#for j in 0..grid[0].size - 1
+            print grid[i][j]
+        end
+    puts ''
+    end
+    puts ''
+end
+
+input = File.new("input.txt","r")
+
+grid = Array.new(200) { Array.new(1000) }
+
+for i in 0..grid.size - 1
+    grid[i].fill('.')
+end
+
+grid[0][500] = 'S'
+
+$window_minx = 1000 #global variables tracking printing window for visual verification
+$window_maxx = 0
+$window_miny = 0
+$window_maxy = -1 #exit condition for part 1
+
+input.each do |line|
+    line = line.delete("\n")
+    raw = line.split(" -> ")
+    for i in 0...raw.size - 1
+        pt1 = raw[i].split(',')
+        pt1[0] = pt1[0].to_i
+        pt1[1] = pt1[1].to_i
+        pt2 = raw[i+1].split(',')
+        pt2[0] = pt2[0].to_i
+        pt2[1] = pt2[1].to_i
+        if pt1[0] < $window_minx || pt2[0] < $window_minx
+            $window_minx = pt1[0] < pt2[0] ? pt1[0] : pt2[0]
+        end
+        if pt1[0] > $window_maxx || pt2[0] > $window_maxx
+            $window_maxx = pt1[0] < pt2[0] ? pt1[0] : pt2[0]
+        end
+
+        if pt1[1] > $window_maxy || pt2[1] > $window_maxy
+            $window_maxy = pt1[1] > pt2[1] ? pt1[1] : pt2[1] 
+        end
+        
+        draw_line(grid,pt1,pt2)
+    end
+end
+
+draw_line(grid,[0,$window_maxy + 2],[grid[0].size-1,$window_maxy + 2])
+
+count = drop_sand(grid)
+
+puts count
